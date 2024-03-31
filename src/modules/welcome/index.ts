f8e489281377c80b735ea7558ed1077ac5440c2e
Root-Time <root.timeuser@protonmail.com>
2024-04-01 00:34:32 +0200

    default role

--- src/modules/welcome/index.ts
+++ src/modules/welcome/index.ts
@@ -10,19 +10,19 @@ import { channel } from "diagnostics_channel";
 import { channels } from "../../lib/get_objects/channels";
 import { Modules } from "../../lib/commands";
 import { log } from "console";
+import { roles } from "../../lib/get_objects/roles";
 
 export default {
   run,
-  commands: []
-} as Modules
+  commands: [],
+} as Modules;
 
 async function run(client: Client) {
-
   client.on(Events.MessageCreate, async (message: Message) => {
     if (message.content === "!test") {
       const image = await createImage(message.author);
 
-      log(image)
+      log(image);
 
       await message.channel.send({ files: [image] });
     }
@@ -31,8 +31,10 @@ async function run(client: Client) {
   client.on(Events.GuildMemberAdd, async (member: GuildMember) => {
     const channel = channels.welcome;
 
+    member.roles.add(roles.default);
+
     const image = await createImage(member.user);
 
     await channel.send({ files: [image] });
   });
-};
+}
