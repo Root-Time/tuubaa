196a7f086764611b9a1f7c1fa1612fbfc7036fca
Root-Time <root.timeuser@protonmail.com>
2024-04-06 02:15:37 +0200

    change

--- src/modules/level/level_up.ts
+++ src/modules/level/level_up.ts
@@ -2,73 +2,72 @@
 
 import { Colors, EmbedBuilder, GuildMember } from "discord.js";
 import { channels } from "../../lib/get_objects/channels";
-import { guild } from "../../lib/get_objects/guild";
 import { Level } from ".";
-import { embed } from "../../lib/embed";
-import { log } from "console";
 import config from "../../config";
-
-
 
 export const LevelUp = {
   xp: addXP,
   embed: levelUpEmebd,
-}
-
+};
 
 async function addXP(member: GuildMember, amount: number) {
-  if (member.user.bot) return
+  if (member.user.bot) return;
 
   // console.log(amount)
-  const levelData = await Level.database.get(member.id)
+  const levelData = await Level.database.get(member.id);
 
   if (!levelData) {
-    await Level.database.set(member.id, amount, 1)
+    await Level.database.set(member.id, amount, 1);
     await channels.bot.send({
       embeds: [
         new EmbedBuilder()
           .setColor(Colors.Green)
-          .setTitle('Level Aufstieg!')
-          .setDescription(`${member}. Du hast deine erste Nachricht geschrieben!\nDu bist nun \n**Level: 1**`)
-      ]
-    })
-    return
+          .setTitle("Level Aufstieg!")
+          .setDescription(
+            `${member}. Du hast deine erste Nachricht geschrieben!\nDu bist nun \n**Level: 1**`
+          ),
+      ],
+    });
+    return;
   }
 
-  let level = levelData.lvl
-  const xp = fix(levelData.xp + amount)
+  let level = levelData.lvl;
+  const xp = fix(levelData.xp + amount);
 
-  const threshold = fix(level ** config.level.exponent * config.level.multiplikator)
-
+  const threshold = fix(
+    level ** config.level.exponent * config.level.multiplikator
+  );
 
   if (xp < threshold) {
-    await Level.database.set(member.id, xp, level)
-    return
+    await Level.database.set(member.id, xp, level);
+    return;
   }
-  const newLevel = level + 1
+  const newLevel = level + 1;
   const newXp = fix(xp - threshold);
 
-  await Level.database.set(member.id, newXp, newLevel)
-  await levelUp(member, newLevel)
+  await Level.database.set(member.id, newXp, newLevel);
+  await levelUp(member, newLevel);
 }
 
 function fix(number: number) {
-  return Math.round((number + Number.EPSILON) * 100) / 100
+  return Math.round((number + Number.EPSILON) * 100) / 100;
 }
 
 function levelUpEmebd(member: GuildMember, level: number) {
   return new EmbedBuilder()
-    .setTitle('Level Aufstieg!')
+    .setTitle("Level Aufstieg!")
     .setColor(Colors.Green)
-    .setDescription(`${member} hat das nächste Level erreicht!\n**Level: ${level}**`)
+    .setDescription(
+      `${member} hat das nächste Level erreicht!\n**Level: ${level}**`
+    )
     .setFooter({
       text: member.displayName,
-      iconURL: member.displayAvatarURL()
-    })
+      iconURL: member.displayAvatarURL(),
+    });
   // .setImage(guild.iconURL())
   // .setThumbnail(guild.iconURL())
 }
 
 async function levelUp(member: GuildMember, level: number) {
-  await channels.bot.send({ embeds: [levelUpEmebd(member, level)] })
+  await channels.bot.send({ embeds: [levelUpEmebd(member, level)] });
 }
